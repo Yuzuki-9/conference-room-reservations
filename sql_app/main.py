46e0234165b789a256756ceaf7365e2202e353f0
Yuzuki-9 <yuzuki96cherry@gmail.com>
2022-05-13 12:45:44 +0900

    create create in main.py

--- sql_app/main.py
+++ sql_app/main.py
@@ -23,6 +23,7 @@ def get_db():
 # async def index():
 #     return {"message": "Success"}
 
+# ----------------------------------------
 # Read
 # 取得したデータをレスポンス（返す）する
 # データはschemas.pyのUserとか
@@ -50,15 +51,16 @@ async def read_bookings(skip: int = 0, limit: int = 100, db: Session = Depends(g
     bookings = crud.get_bookings(db, skip=skip, limit=limit)
     return bookings
 
+# ----------------------------------------
 # Create
-@app.post("/users")
-async def users(users: User):
-    return {"users": users}
+@app.post("/users", response_model=schemas.User)  # 作成→1つのデータ
+async def create_user(user: schemas.User, db: Session = Depends(get_db)):
+    return crud.create_user(db=db, user=user)
 
-@app.post("/bookings")
-async def bookings(bookings: Booking):
-    return {"bookings": bookings}
+@app.post("/rooms", response_model=schemas.Room)  # 作成→1つのデータ
+async def create_room(room: schemas.Room, db: Session = Depends(get_db)):
+    return crud.create_room(db=db, room=room)
 
-@app.post("/rooms")
-async def rooms(rooms: Room):
-    return {"rooms": rooms}
+@app.post("/bookings", response_model=schemas.Booking)  # 作成→1つのデータ
+async def create_booking(booking: schemas.Booking, db: Session = Depends(get_db)):
+    return crud.create_booking(db=db, booking=booking)
