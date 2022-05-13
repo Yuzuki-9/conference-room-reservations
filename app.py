048040f1c1e09c157bc7e99ad0d51223e85b8984
Yuzuki-9 <yuzuki96cherry@gmail.com>
2022-05-13 13:16:11 +0900

    create room registration screen

--- app.py
+++ app.py
@@ -24,8 +24,6 @@ if page == 'users':
 
     # submit_buttonが押されたとき（リクエスト送信されたとき）
     if submit_button:
-        st.write('## 送信データ')
-        st.write('## レスポンス結果')
         url = 'http://127.0.0.1:8000/users'
         res = requests.post(
             url,
@@ -39,32 +37,31 @@ if page == 'users':
 
 # 会議室画面
 elif page == 'rooms':
-    st.title('APIテスト画面（会議室）')
+    st.title('会議室登録画面')
 
     # formの中身にどういう項目を入れるのか→withの中身で指定する
     with st.form(key='room'):  # keyはformとの紐付け
-        room_id: int = random.randint(0, 10)  # room_idは登録したタイミングで決める
+        # room_id: int = random.randint(0, 10)  # room_idは登録したタイミングで決める
         room_name: str = st.text_input('会議室名', max_chars=12)  # , max12文字
         capacity: int = st.number_input('定員', step=1)
         # roomのpostで必要なデータを送ってあげる
         data = {
-            'room_id': room_id,
+            # 'room_id': room_id,
             'room_name': room_name,
             'capacity': capacity
         }
-        submit_button = st.form_submit_button(label='リクエスト送信')  # formに紐づく送信ボタン
+        submit_button = st.form_submit_button(label='会議室登録')  # formに紐づく送信ボタン
 
     # submit_buttonが押されたとき（リクエスト送信されたとき）
     if submit_button:
-        st.write('## 送信データ')
-        st.json(data)
-        st.write('## レスポンス結果')
         url = 'http://127.0.0.1:8000/rooms'
         res = requests.post(
             url,
             data=json.dumps(data)
         )
-        st.write(res.status_code)
+        if res.status_code == 200:
+            st.success('会議室登録完了')
+        # st.write(res.status_code)
         st.json(res.json())
 
 
